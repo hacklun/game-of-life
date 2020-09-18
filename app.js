1176df28be989726d9e0b0e302a2f3f21b7e67c0
hacklun <hacklun@mail.ru>
2020-09-18 13:00:48 +0300

    add game loop

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 /*
-* Game of Life v0.2
+* Game of Life v0.3
 * Author: Ivshin Pavel aka hacklun
 */
 
 const canvas = document.querySelector('#board canvas');
 const ctx = canvas.getContext('2d');
+
+// Game setup
+
+// Game loop
+const timer = setInterval(() => {
+
+}, 1000 / 60);
