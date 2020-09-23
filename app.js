f832c768b3c7ac61e3bf2cce88c92f9b9da5123c
hacklun <hacklun@mail.ru>
2020-09-23 14:42:05 +0300

    draw first generation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ctx = canvas.getContext('2d');
 
 // Game setup
 let cells = [];
+const SIZE_CELL = 30;
 for (let i = 0; i < 10; i++) {
     let row = [];
     for (let j = 0; j < 10; j++) {
@@ -15,10 +16,24 @@ for (let i = 0; i < 10; i++) {
     }
     cells.push(row);
 }
+ctx.strokeStyle = "gray";
 
-console.log(cells);
+const draw = () => {
+    let y = 0;
+    for (let i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            if (cells[i][j] === 1) {
+                ctx.fillRect(j * SIZE_CELL, y, SIZE_CELL, SIZE_CELL);
+            }
+            ctx.strokeRect(j * SIZE_CELL, y, SIZE_CELL, SIZE_CELL);
+        }
+        y += SIZE_CELL;
+    }
+}
+console.log(cells)
+
 
 // Game loop
 const timer = setInterval(() => {
-
+    draw();
 }, 1000 / 60);
