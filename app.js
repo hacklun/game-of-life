9556768cf91f19aadd5aad611fff53ffa5c1d615
hacklun <hacklun@mail.ru>
2020-09-23 12:44:24 +0300

    fill array cells (first generation)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-* Game of Life v0.4
+* Game of Life v0.5
 * Author: Ivshin Pavel aka hacklun
 */
 
@@ -8,6 +8,15 @@ const ctx = canvas.getContext('2d');
 
 // Game setup
 let cells = [];
+for (let i = 0; i < 10; i++) {
+    let row = [];
+    for (let j = 0; j < 10; j++) {
+        row.push(Math.round(Math.random()))
+    }
+    cells.push(row);
+}
+
+console.log(cells);
 
 // Game loop
 const timer = setInterval(() => {
